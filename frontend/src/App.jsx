8cfb68b490502fcfd7eba551afeffd07fd943547
devrajpatel1006 <devrajpatel1006@gmail.com>
2024-01-12 21:54:31 +0530

    commit src folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { OrderList } from './components/OrderList'
+
+function App() {
+
+  return (
+    <div className='app'>
+        <OrderList />
+    </div>
+  )
+}
+
+export default App
